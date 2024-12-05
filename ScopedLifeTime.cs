e06e1edb31cc6ee97f6b3e6b8e3c4bda6c4b533e
AnshV-optimus <ansh.verma@optimusinfo.com>
2024-12-05 18:46:08 +0530

    Add files via upload

---
+++ ScopedLifeTime.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dotnet
+{
+    public interface IScoped
+    {
+        void GetData();
+        Guid guid();
+    }
+
+    public interface ITranisent
+    {
+        public Guid _guid { get; }
+    }
+
+    public class EmployeeDetails : IScoped
+    {
+        Guid _guid = Guid.NewGuid();
+        public void GetData()
+        {
+            Console.WriteLine("Data fetched from DB");
+        }
+        public Guid guid()
+        {
+            return _guid;
+        }
+    }
+
+
+    public class OrderDetails : ITranisent
+    {
+        public Guid _guid { get; set; }
+
+        public OrderDetails()
+        {
+            _guid = Guid.NewGuid();
+        }
+        public Guid guid()
+        {
+            return _guid;
+        }
+    }
+
+    public class ScopedClient
+    {
+        IScoped scoped;
+        public ScopedClient(IScoped scoped)
+        {
+            this.scoped = scoped;
+        }
+        public void Print()
+        {
+            scoped.GetData();
+        }
+        public Guid guid()
+        {
+            return scoped.guid();
+        }
+
+    }
+
+    public class TransientClient
+    {
+        ITranisent transient;
+
+        public TransientClient(ITranisent transient)
+        {
+            this.transient = transient;
+        }
+
+        public Guid GetInstance()
+        {
+            return transient._guid;
+        }
+    }
+
+    internal class ScopedLifeTime
+    {
+        public static void Main(string[] args)
+        {
+            var Services = new ServiceCollection()
+                .AddScoped<IScoped, EmployeeDetails>()
+                .AddScoped<ScopedClient>()
+                .AddTransient<ITranisent, OrderDetails>()
+                .AddTransient<TransientClient>()
+                .BuildServiceProvider();
+
+            using (var scope = Services.CreateScope())
+            {
+                // Resolve ScopedClient within the scope
+                var GetService = scope.ServiceProvider.GetService<ScopedClient>();
+                var GetService2 = scope.ServiceProvider.GetService<ScopedClient>();
+
+                GetService.Print();
+                Guid instanceId = GetService.guid();
+                Guid instanceId1 = GetService2.guid();
+
+                Console.WriteLine("Scoped Service GUIDs:");
+                Console.WriteLine(instanceId);
+                Console.WriteLine(instanceId1);
+
+                Console.WriteLine("Transient Service GUIDs:");
+                var TransientService1 = scope.ServiceProvider.GetService<TransientClient>();
+                var TransientService2 = scope.ServiceProvider.GetService<TransientClient>();
+                var TransientId1 = TransientService1.GetInstance();
+                var TransientId2 = TransientService2.GetInstance();
+
+                Console.WriteLine(TransientId1 == TransientId2);
+                Console.WriteLine(TransientId1);
+                Console.WriteLine(TransientId2);
+            }
+
+            Console.WriteLine();
+
+            using (var scope = Services.CreateScope())
+            {
+                // Resolve ScopedClient within the new scope
+                var GetService = scope.ServiceProvider.GetService<ScopedClient>();
+                var GetService2 = scope.ServiceProvider.GetService<ScopedClient>();
+
+                GetService.Print();
+                Guid instanceId = GetService.guid();
+                Guid instanceId1 = GetService2.guid();
+
+                Console.WriteLine("Scoped Service GUIDs (new scope):");
+                Console.WriteLine(instanceId);
+                Console.WriteLine(instanceId1);
+
+                Console.WriteLine("Transient Service GUIDs (new scope):");
+                var TransientService = scope.ServiceProvider.GetService<TransientClient>();
+                var TransientService2 = scope.ServiceProvider.GetService<TransientClient>();
+                var TransientId1 = TransientService.GetInstance();
+                var TransientId2 = TransientService2.GetInstance();
+                Console.WriteLine(TransientId1);
+                Console.WriteLine(TransientId2);
+            }
+        }
+
+
+    }
+}
---
+++ SingletonLifetime.cs
@@ -0,0 +1,129 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.DependencyInjection;
+
+//namespace Dotnet
+//{
+
+//    public class orders
+//    {
+//        public int id { get; set; }
+//        public string name { get; set; }
+
+//    }
+//    public interface IOrders
+//    {
+//        List<orders> GetOrders();
+//    }
+
+//    public interface IGuid
+//    {
+//        Guid GetInstanceId();
+//    }
+
+//    public class OrderDetails : IOrders, IGuid
+//    {
+//        Guid _instanceId = Guid.NewGuid();
+
+
+//        public List<orders> GetOrders()
+//        {
+//            List<orders> orderlist = new List<orders>()
+//            {
+//                new orders {id = 1 , name = "Pencil"},
+//                new orders {id = 2 , name = "Eraser" },
+//                new orders {id = 3 , name = "Box" }
+//            };
+
+//            return orderlist;
+//        }
+
+//        public Guid GetInstanceId()
+//        {
+//            return _instanceId;
+//        }
+
+
+//        public class Cart
+//        {
+//            private readonly IOrders _orderDetails;
+//            private readonly IGuid _instanceId;
+//            public Cart(IOrders orderDetails, IGuid _instanceId)
+//            {
+//                this._orderDetails = orderDetails;
+//                this._instanceId = _instanceId;
+//            }
+
+//            public List<orders> Print()
+//            {
+//                return _orderDetails.GetOrders();
+//            }
+
+//            public Guid GetGuid()
+//            {
+//                return _instanceId.GetInstanceId();
+//            }
+//        }
+
+//        public class Cart1
+//        {
+//            private readonly IOrders _orderDetails;
+//            private  IGuid _instanceId;
+//            public Cart1(IOrders orderDetails, IGuid _instanceId)
+//            {
+//                this._orderDetails = orderDetails;
+//                this._instanceId = _instanceId;
+//            }
+
+//            public List<orders> Print()
+//            {
+//                return _orderDetails.GetOrders();
+//            }
+
+//            public Guid GetGuid()
+//            {
+//                return _instanceId.GetInstanceId();
+//            }
+//        }
+
+
+//        internal class SingletonLifetime
+//        {
+//            public static void Main(string[] args)
+//            {
+//                var services = new ServiceCollection();
+
+//                services.AddSingleton<IOrders, OrderDetails>();
+//                services.AddSingleton<IGuid, OrderDetails>();
+//                //services.AddSingleton<Cart>();
+
+
+//                var serviceProvider = services.BuildServiceProvider();
+
+//                var cart = new Cart(new OrderDetails(), new OrderDetails());
+//                var cart1 = new Cart1(new OrderDetails(), new OrderDetails());
+                
+//                cart1.GetGuid();
+
+//                Console.WriteLine(cart.GetGuid());
+//                Console.WriteLine(cart1.GetGuid());
+
+//                //var GetService = new Cart(new OrderDetails() , new OrderDetails());
+
+//                //var result = GetService.Print();
+//                //Console.WriteLine(GetService.GetGuid());
+
+//                //foreach (var item in result)
+//                //{
+//                //    Console.WriteLine(item.name);
+//                //}
+
+
+
+//            }
+//        }
+//    }
+//}
